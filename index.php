b75ac4b9b0b8d335a662c30bcdedc2b74b6bf121
theblackgigant <reyno.tekoppele@gmail.com>
2013-09-09 14:21:45 +0200

    bende.htmlcssphpjs

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     </head>
     <body>
         <div id="container">
-        	<div id="header"></div>
+        	<div id="header"><img id="logo" src="images/logo.png"><img id="quote" src="images/Quote.png"></div>
         	<div id="menu"></div>
         	<div id="content"></div>
         	<div id="footer"></div>	
