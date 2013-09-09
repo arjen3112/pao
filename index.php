ba5171c9d334383065c85346973897a485372979
arjen3112 <arjen.bovee@live.nl>
2013-09-09 15:42:35 +0200

    menu

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <html>
     <head>
-        <link rel="stylesheet" type="text/css" href="style.css" />
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+        <link rel="stylesheet" type="text/css" href="css/menustyle.css" />
     </head>
     <body>
         <div id="container">
         	<div id="header"><img id="logo" src="images/logo.png"><img id="quote" src="images/Quote.png"></div>
-        	<div id="menu"></div>
+        	<div id="menu">
+        	   <div id="menuhomepage">Homepage</div>
+        	   <div id="menuinformatie">Informatie</div>
+        	   <div id="menuwebshop">Webshop</div>
+        	   <div id="menuinloggen">Inloggen</div>
+        	</div>
         	<div id="content"></div>
         	<div id="footer"></div>	
         </div>
