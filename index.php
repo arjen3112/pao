136fe706a482d190a0c3ceab3b68df883edc0fc4
theblackgigant <reyno.tekoppele@gmail.com>
2013-09-16 11:01:58 +0200

    js voor menu

--- index.php
+++ index.php
@@ -8,10 +8,10 @@
         <div id="container">
         	<div id="header"><img id="logo" src="images/logo.png"><img id="quote" src="images/Quote.png"></div>
         	<div id="menu">
-        	   <div id="menuhomepage">Homepage</div>
-        	   <div id="menuinformatie">Informatie</div>
-        	   <div id="menuwebshop">Webshop</div>
-        	   <div id="menuinloggen">Inloggen</div>
+        	   <div id="menuhomepage" onclick="menu(this)">Homepage</div>
+        	   <div id="menuinformatie" onclick="menu(this)">Informatie</div>
+        	   <div id="menuwebshop" onclick="menu(this)">Webshop</div>
+        	   <div id="menuinloggen" onclick="menu(this)">Inloggen</div>
         	</div>
         	<div id="content"></div>
         	<div id="footer"></div>	
