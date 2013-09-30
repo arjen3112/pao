2fafd3c373df437b5a980588f41158989a35ee72
arjen3112 <arjen.bovee@live.nl>
2013-09-30 10:50:07 +0200

    menu veranderd bij inloggen

--- functions/menu.php
+++ functions/menu.php
@@ -6,49 +6,68 @@ function menu(){
             case 'homepage' :
                 $menu='<a href="?menuoptie=homepage"><div id="menuhomepage" class="menuGeselecteerd">Homepage</div></a>
                        <a href="?menuoptie=informatie"><div id="menuinformatie" class="">Informatie</div></a>
-                       <a href="?menuoptie=webshop"><div id="menuwebshop" class="">Webshop</div></a>
-                       <a href="?menuoptie=inloggen"><div id="menuinloggen" class="">Inloggen</div></a>
-              ';
+                       <a href="?menuoptie=webshop"><div id="menuwebshop" class="">Webshop</div></a>';
+                       if(isset($_SESSION["profiel"]) && $_SESSION["profiel"]=="1"){
+                            $menu.='<a href="?menuoptie=inloggen"><div id="menuinloggen" class="">Account</div></a>';
+                       }else{
+                            $menu.='<a href="?menuoptie=inloggen"><div id="menuinloggen" class="">Inloggen</div></a>'; 
+                       }
                 break;
             
             case 'informatie' :
                 $menu='<a href="?menuoptie=homepage"><div id="menuhomepage" class="">Homepage</div></a>
                        <a href="?menuoptie=informatie"><div id="menuinformatie" class="menuGeselecteerd">Informatie</div></a>
-                       <a href="?menuoptie=webshop"><div id="menuwebshop" class="">Webshop</div></a>
-                       <a href="?menuoptie=inloggen"><div id="menuinloggen" class="">Inloggen</div></a>
-              ';
+                       <a href="?menuoptie=webshop"><div id="menuwebshop" class="">Webshop</div></a>';
+                       if(isset($_SESSION["profiel"]) && $_SESSION["profiel"]=="1"){
+                            $menu.='<a href="?menuoptie=inloggen"><div id="menuinloggen" class="">Account</div></a>';
+                       }else{
+                            $menu.='<a href="?menuoptie=inloggen"><div id="menuinloggen" class="">Inloggen</div></a>'; 
+                       }
                 break;
                 
             case 'webshop' :
                 $menu='<a href="?menuoptie=homepage"><div id="menuhomepage" class="">Homepage</div></a>
                        <a href="?menuoptie=informatie"><div id="menuinformatie" class="">Informatie</div></a>
-                       <a href="?menuoptie=webshop"><div id="menuwebshop" class="menuGeselecteerd">Webshop</div></a>
-                       <a href="?menuoptie=inloggen"><div id="menuinloggen" class="">Inloggen</div></a>
-              ';
+                       <a href="?menuoptie=webshop"><div id="menuwebshop" class="menuGeselecteerd">Webshop</div></a>';
+                       if(isset($_SESSION["profiel"]) && $_SESSION["profiel"]=="1"){
+                            $menu.='<a href="?menuoptie=inloggen"><div id="menuinloggen" class="">Account</div></a>';
+                       }else{
+                            $menu.='<a href="?menuoptie=inloggen"><div id="menuinloggen" class="">Inloggen</div></a>'; 
+                       }
                 break;
             
             case 'inloggen' :
                 $menu='<a href="?menuoptie=homepage"><div id="menuhomepage" class="">Homepage</div></a>
                        <a href="?menuoptie=informatie"><div id="menuinformatie" class="">Informatie</div></a>
-                       <a href="?menuoptie=webshop"><div id="menuwebshop" class="">Webshop</div></a>
-                       <a href="?menuoptie=inloggen"><div id="menuinloggen" class="menuGeselecteerd">Inloggen</div></a>
-              ';
+                       <a href="?menuoptie=webshop"><div id="menuwebshop" class="">Webshop</div></a>';
+                       if(isset($_SESSION["profiel"]) && $_SESSION["profiel"]=="1"){
+                            $menu.='<a href="?menuoptie=inloggen"><div id="menuinloggen" class="menuGeselecteerd">Account</div></a>';
+                       }else{
+                            $menu.='<a href="?menuoptie=inloggen"><div id="menuinloggen" class="menuGeselecteerd">Inloggen</div></a>'; 
+                       }
                 break;
                 
             default :
                 $menu='<a href="?menuoptie=homepage"><div id="menuhomepage" class="menuGeselecteerd">Homepage</div></a>
                        <a href="?menuoptie=informatie"><div id="menuinformatie" class="">Informatie</div></a>
-                       <a href="?menuoptie=webshop"><div id="menuwebshop" class="">Webshop</div></a>
-                       <a href="?menuoptie=inloggen"><div id="menuinloggen" class="">Inloggen</div></a>
-              ';
+                       <a href="?menuoptie=webshop"><div id="menuwebshop" class="">Webshop</div></a>';
+                       if(isset($_SESSION["profiel"]) && $_SESSION["profiel"]=="1"){
+                            $menu.='<a href="?menuoptie=inloggen"><div id="menuinloggen" class="">Account</div></a>';
+                       }else{
+                            $menu.='<a href="?menuoptie=inloggen"><div id="menuinloggen" class="">Inloggen</div></a>'; 
+                       }
+                       
                 break;
         }
     }else{
        $menu='<a href="?menuoptie=homepage"><div id="menuhomepage" class="menuGeselecteerd">Homepage</div></a>
                        <a href="?menuoptie=informatie"><div id="menuinformatie" class="">Informatie</div></a>
-                       <a href="?menuoptie=webshop"><div id="menuwebshop" class="">Webshop</div></a>
-                       <a href="?menuoptie=inloggen"><div id="menuinloggen" class="">Inloggen</div></a>
-              '; 
+                       <a href="?menuoptie=webshop"><div id="menuwebshop" class="">Webshop</div></a>';
+                       if(isset($_SESSION["profiel"]) && $_SESSION["profiel"]=="1"){
+                            $menu.='<a href="?menuoptie=inloggen"><div id="menuinloggen" class="">Account</div></a>';
+                       }else{
+                            $menu.='<a href="?menuoptie=inloggen"><div id="menuinloggen" class="">Inloggen</div></a>'; 
+                       }
     }
     return $menu;
     
