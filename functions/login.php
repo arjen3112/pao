2ca88f2a843557ff1363ae748a1dd397ad2eea18
theblackgigant <reyno.tekoppele@gmail.com>
2013-10-07 11:39:36 +0200

    registratie vergroot met heel wat gegevens

--- functions/login.php
+++ functions/login.php
@@ -213,6 +213,76 @@ function getregistratie()
 			}
 			$output.='/></td>
     		</tr>
+    		<tr>
+    			<td>Adres: </td>
+    			<td><input name="adres" type="text" placeholder="Straatnaam"
+			';
+			if(isset($_POST['adres']))
+			{
+				$output .= " value=\"". $_POST['adres'] ."\"";
+				$adres = $_POST['adres'];
+				$pattern = '/^[A-Z]{1}[a-z]{4,500}$/';
+				if(!preg_match($pattern,$adres))
+				{
+					$output.= "style=\"border:2px solid red;\" ";
+					$check=1;
+				}
+			}
+			$output.='/></td>
+    		</tr>
+    		<tr>
+    			<td>Huisnummer: </td>
+    			<td><input name="huisnummer" type="text" placeholder="Huisnummer"
+			';
+			if(isset($_POST['huisnummer']))
+			{
+				$output .= " value=\"". $_POST['huisnummer'] ."\"";
+				$huisnummer = $_POST['huisnummer'];
+				$pattern = '/^.*[1-9][0-9]*$/';
+				if(!preg_match($pattern,$huisnummer))
+				{
+					$output.= "style=\"border:2px solid red;\" ";
+					$check=1;
+				}
+			}
+			$output.='/></td>
+    		</tr>
+    		<tr>
+    			<td>Postcode: </td>
+    			<td><input name="postcode" type="text" placeholder="1234 AB"
+			';
+			if(isset($_POST['postcode']))
+			{
+				$output .= " value=\"". $_POST['postcode'] ."\"";
+				$postcode = $_POST['postcode'];
+				$pattern = '/^[1-9][0-9]{3}[\s]?[A-Za-z]{2}$/i';
+				if(!preg_match($pattern,$postcode))
+				{
+					$output.= "style=\"border:2px solid red;\" ";
+					$check=1;
+				}				
+			}
+			$output.='/></td>
+    		</tr>
+    		<tr>
+    			<td>Telefoon: </td>
+    			<td><input name="telefoon" type="text" placeholder="0123-123456"
+			';
+			if(isset($_POST['telefoon']))
+			{
+				$output .= " value=\"". $_POST['telefoon'] ."\"";
+				$telefoon = $_POST['telefoon'];
+				$telefoon = str_replace("-", "", $telefoon);
+				$telefoon = str_replace(" ", "", $telefoon);
+				$pattern = '/^[0-9]{4,12}$/';
+				if(!preg_match($pattern,$telefoon))
+				{
+					$output.= "style=\"border:2px solid red;\" ";
+					$check=1;
+				}				
+			}
+			$output.='/></td>
+    		</tr>
     		<tr><td colspan="2">
     		<input type="submit" name="submitregistreren" class="button" value="registreer">
     		</td></tr>
@@ -223,6 +293,10 @@ function getregistratie()
 		    $voor = $_POST['name'];
             $wacht = $_POST['password'];
             $email = $_POST['email'];
+			$adres = $_POST['adres'];
+			$huisnummer = $_POST['huisnummer'];
+			$postcode = $_POST['postcode'];
+			$telefoon = $_POST['telefoon'];
             
 		    $query = mysql_query('SELECT * FROM `accounts` WHERE `account` ="'.$voor.'" OR `email` ="'.$email.'"');   
             $nummer_rows=mysql_num_rows($query);
@@ -232,9 +306,9 @@ function getregistratie()
             }            
 			if(empty($check)){
     			$q = "INSERT INTO accounts
-    					(account, password, email, profiel)
+    					(account, password, email, profiel, adres, huisnummer, postcode, telefoon)
     					VALUES
-    					('$voor','$wacht','$email','2')";
+    					('$voor','$wacht','$email','2','$adres','$huisnummer','$postcode','$telefoon')";
     			mysql_query($q);
                 $output = '
 			        <table id="tableLogin">
@@ -273,67 +347,133 @@ function registratieBestaat(){
                 <td>Gebruikersnaam: </td>
                 <td><input name="name" type="text" placeholder="Typ hier je naam"
             ';
-            if(isset($_POST['name']))
-            {
-                $output .= " value=\"". $_POST['name'] ."\" ";
-                $name = $_POST['name'];
-                $pattern = '/^.*[a-zA-Z]$/';
-                if(!preg_match($pattern,$name))
-                {
-                    $output.= "style=\"border:2px solid red;\" ";
-                    $check=1;
-                }
-            }
-            $output.='/></td>
-            </tr>
-            <tr>
-                <td>Wachwoord: </td>
-                <td><input name="password" type="password" placeholder="Typ hier je wachtwoord"
-            ';
-            if(isset($_POST['password']))
-            {
-                $output .= " value=\"". $_POST['password'] ."\"";
-                $password = $_POST['password'];
-                $pattern = '/^.*[a-zA-Z][0-9]$/';
-                if(!preg_match($pattern,$password))
-                {
-                    $output.= "style=\"border:2px solid red;\" ";
-                    $check=1;
-                }
-            }
-            $output.='/></td>
-            </tr>
-            <tr>
-                <td>Wachwoord herhalen: </td>
-                <td><input name="password2" type="password" placeholder="Typ hier je wachtwoord"
-            ';
-            if(isset($_POST['password2']))
-            {
-                $output .= " value=\"". $_POST['password2'] ."\"";
-                $password2 = $_POST['password2'];
-                $pattern = $_POST['password'];
-                if($pattern !== $password2)
-                {
-                    $output.= "style=\"border:2px solid red;\" ";
-                    $check=1;
-                }
-            }
-            $output.='/></td>
-            </tr>
-            <tr>
-                <td>Emailadres: </td>
-                <td><input name="email" type="text" placeholder="bakker@example.com"
-            ';
-            if(isset($_POST['email']))
-            {
-                $output .= " value=\"". $_POST['email'] ."\"";
-                $email = $_POST['email'];
-                if(!filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL))
-                {
-                    $output.= "style=\"border:2px solid red;\" ";
-                    $check=1;
-                }
-            }
+           if(isset($_POST['name']))
+			{
+				$output .= " value=\"". $_POST['name'] ."\" ";
+				$name = $_POST['name'];
+				$pattern = '/^.*[a-zA-Z]$/';
+				if(!preg_match($pattern,$name))
+				{
+					$output.= "style=\"border:2px solid red;\" ";
+					$check=1;
+				}
+			}
+			$output.='/></td>
+    		</tr>
+    		<tr>
+    			<td>Wachwoord: </td>
+    			<td><input name="password" type="password" placeholder="Typ hier je wachtwoord"
+			';
+			if(isset($_POST['password']))
+			{
+				$output .= " value=\"". $_POST['password'] ."\"";
+				$password = $_POST['password'];
+				$pattern = '/^.*[a-zA-Z][0-9]$/';
+				if(!preg_match($pattern,$password))
+				{
+					$output.= "style=\"border:2px solid red;\" ";
+					$check=1;
+				}
+			}
+			$output.='/></td>
+    		</tr>
+    		<tr>
+    			<td>Wachwoord herhalen: </td>
+    			<td><input name="password2" type="password" placeholder="Typ hier je wachtwoord"
+			';
+			if(isset($_POST['password2']))
+			{
+				$output .= " value=\"". $_POST['password2'] ."\"";
+				$password2 = $_POST['password2'];
+				$pattern = $_POST['password'];
+				if($pattern !== $password2)
+				{
+					$output.= "style=\"border:2px solid red;\" ";
+					$check=1;
+				}
+			}
+			$output.='/></td>
+    		</tr>
+    		<tr>
+    			<td>Emailadres: </td>
+    			<td><input name="email" type="text" placeholder="bakker@example.com"
+			';
+			if(isset($_POST['email']))
+			{
+				$output .= " value=\"". $_POST['email'] ."\"";
+				$email = $_POST['email'];
+				if(!filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL))
+				{
+					$output.= "style=\"border:2px solid red;\" ";
+					$check=1;
+				}
+			}
+			$output.='/></td>
+    		</tr>
+    		<tr>
+    			<td>Adres: </td>
+    			<td><input name="adres" type="text" placeholder="Straatnaam"
+			';
+			if(isset($_POST['adres']))
+			{
+				$output .= " value=\"". $_POST['adres'] ."\"";
+				$adres = $_POST['adres'];
+				$pattern = '/^[A-Z]{1}[a-z]{4,500}$/';
+				if(!preg_match($pattern,$adres))
+				{
+					$output.= "style=\"border:2px solid red;\" ";
+				}
+			}
+			$output.='/></td>
+    		</tr>
+    		<tr>
+    			<td>Huisnummer: </td>
+    			<td><input name="huisnummer" type="text" placeholder="Huisnummer"
+			';
+			if(isset($_POST['huisnummer']))
+			{
+				$output .= " value=\"". $_POST['huisnummer'] ."\"";
+				$huisnummer = $_POST['huisnummer'];
+				$pattern = '/^.*[1-9][0-9]*$/';
+				if(!preg_match($pattern,$huisnummer))
+				{
+					$output.= "style=\"border:2px solid red;\" ";
+				}
+			}
+			$output.='/></td>
+    		</tr>
+    		<tr>
+    			<td>Postcode: </td>
+    			<td><input name="postcode" type="text" placeholder="1234 AB"
+			';
+			if(isset($_POST['postcode']))
+			{
+				$output .= " value=\"". $_POST['postcode'] ."\"";
+				$postcode = $_POST['postcode'];
+				$pattern = '/^[1-9][0-9]{3}[\s]?[A-Za-z]{2}$/i';
+				if(!preg_match($pattern,$postcode))
+				{
+					$output.= "style=\"border:2px solid red;\" ";
+				}				
+			}
+			$output.='/></td>
+    		</tr>
+    		<tr>
+    			<td>Telefoon: </td>
+    			<td><input name="telefoon" type="text" placeholder="0123-123456"
+			';
+			if(isset($_POST['telefoon']))
+			{
+				$output .= " value=\"". $_POST['telefoon'] ."\"";
+				$telefoon = $_POST['telefoon'];
+				$telefoon = str_replace("-", "", $telefoon);
+				$telefoon = str_replace(" ", "", $telefoon);
+				$pattern = '/^[0-9]{4,12}$/';
+				if(!preg_match($pattern,$telefoon))
+				{
+					$output.= "style=\"border:2px solid red;\" ";
+				}				
+			}
             $output.='/></td>
             </tr>
             <tr><td colspan="2">
@@ -346,12 +486,25 @@ function registratieBestaat(){
 
 function getaccountgegevens()
 {
-    $output= "hallo";
-    return $output;
+     $output = '
+        <table id="tableLogin">
+        <form method="post" action="?menuoptie=inloggen">
+            <tr>
+                <td>Gebruikersnaam: </td>
+                <td> '.$_SESSION['username'].'</td>
+            </tr>
+            <tr>
+                <td>Wachtwoord: </td>
+                <td>Staat hier</td>
+            </tr>
+        </form>
+        </table>
+        ';
+        return $output;
 }
 
 function getbestellingen()
 {
-    $output= "hallo2";
+    $output= "Bestellingsscherm binnenkort beschikbaar";
     return $output;
 } 
