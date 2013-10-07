caaf313f9058054aa9340c0621c3978a0e3c2364
theblackgigant <reyno.tekoppele@gmail.com>
2013-10-07 15:06:01 +0200

    sql update

--- database/pao.sql
+++ database/pao.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Machine: localhost
--- Genereertijd: 23 sep 2013 om 09:19
+-- Genereertijd: 07 okt 2013 om 13:05
 -- Serverversie: 5.5.24-log
 -- PHP-versie: 5.3.13
 
@@ -32,15 +32,19 @@ CREATE TABLE IF NOT EXISTS `accounts` (
   `password` varchar(15) DEFAULT NULL,
   `email` varchar(40) DEFAULT NULL,
   `profiel` int(1) DEFAULT NULL,
+  `adres` varchar(50) DEFAULT NULL,
+  `huisnummer` varchar(7) DEFAULT NULL,
+  `postcode` varchar(7) DEFAULT NULL,
+  `telefoon` varchar(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=21 ;
 
 --
 -- Gegevens worden uitgevoerd voor tabel `accounts`
 --
 
-INSERT INTO `accounts` (`id`, `account`, `password`, `email`, `profiel`) VALUES
-(1, 'Administrator', 'admin', 'admin@admin.com', 1);
+INSERT INTO `accounts` (`id`, `account`, `password`, `email`, `profiel`, `adres`, `huisnummer`, `postcode`, `telefoon`) VALUES
+(1, 'Administrator', 'admin', 'admin@admin.com', 1, 'localhost', '1', '1234 AB', '0123-123456');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
