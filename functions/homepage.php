7b9108b9517605c0b046b89c3417081885c741d1
theblackgigant <reyno.tekoppele@gmail.com>
2013-11-06 10:23:55 +0100

    een error

--- functions/homepage.php
+++ functions/homepage.php
@@ -8,10 +8,17 @@ function homepage(){
 	$resultaat = mysql_query($query);
 	$row = mysql_fetch_array($resultaat);
 	
+	$q = 'SELECT * FROM `content`
+		WHERE `pagina` = "homepage"
+		AND `type` = "image"';
+		
+	$r = mysql_query($q);
+	$rw = mysql_fetch_array($r);
+	
 	$output='
 	<div id="infohomepage">'.$row["content"].'</div>
-	<div id="imghome1"><img src="images/homepage/vlinder.jpg"></div>
-	<div id="imghome2"><img src="images/homepage/kat.jpg"></div>';
+	<div id="imghome1"><img src="'.$r.'"></div>
+	<div id="imghome2"><img src="'.$r.'"></div>';
 	
 	return $output;
 }
