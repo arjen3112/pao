0168a76884f8b15dbf717514935cec4dc52a498f
theblackgigant <reyno.tekoppele@gmail.com>
2013-11-06 09:56:59 +0100

    homepage almost klaar met laden uit SQL

--- functions/homepage.php
+++ functions/homepage.php
@@ -2,14 +2,14 @@
 function homepage(){
 
 	$query = 'SELECT * FROM `content`
-		WHERE `pagina`,`content`
+		WHERE `pagina` = "homepage"
 		AND `type` = "text"';
 		
 	$resultaat = mysql_query($query);
 	$row = mysql_fetch_array($resultaat);
-
+	
 	$output='
-	<div id="infohomepage">'.$resultaat.'</div>
+	<div id="infohomepage">'.$row["content"].'</div>
 	<div id="imghome1"><img src="images/homepage/vlinder.jpg"></div>
 	<div id="imghome2"><img src="images/homepage/kat.jpg"></div>';
 	
