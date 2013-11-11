6fbfc93bb03445e3c942a8ecfb4c13aa61393959
theblackgigant <reyno.tekoppele@gmail.com>
2013-11-11 14:53:00 +0100

    KUT stijl gedoe werkt niet meee!

--- functions/homepage.php
+++ functions/homepage.php
@@ -1,28 +1,31 @@
 <?php
-function homepage(){
+function homepage() {
 
 	$query = 'SELECT * FROM `content`
 		WHERE `pagina` = "homepage"
 		AND `type` = "text"';
-		
+
 	$resultaat = mysql_query($query);
 	$row = mysql_fetch_array($resultaat);
-	
+
 	$q = 'SELECT `content` FROM `content`
-		WHERE `pagina` = "homepage"
-		AND `type` = "image"
-		AND `id`= "2" OR `id` = "3"';
-		
+			WHERE `pagina` = "homepage"
+			AND `type` = "image"
+			AND `id`= "2" OR `id` = "3"';
+
 	$r = mysql_query($q);
-		
-	$output='
-	<div id="infohomepage">'.$row["content"].'</div>
-	';
-	$i=1;
+
+	$output = '
+		<div id="infohomepage">' . $row["content"] . '</div>
+		';
+	$i = 1;
 	while ($rw = mysql_fetch_array($r)) {
-		$output.='<div id="imghome'.$i.'"><img src="'.$rw["content"].'"></div>';
+		$output .= '<div id="imghome' . $i . '"><img src="' . $rw["content"] . '"></div>';
 		$i++;
 	}
-	
+
+	if (isset($_SESSION["profiel"]) && $_SESSION["profiel"] == "1") {
+		$output.='<input type="submit" class="buttonHomepage" value="Upload afbeelding">';
+	}
 	return $output;
 }
