f26f383144883fdd37daf2f6cdb0801e7bdc8fc7
arjen3112 <arjen.bovee@live.nl>
2013-11-29 08:35:34 +0100

    database geupdate

--- database/pao.sql
+++ database/pao.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Machine: localhost
--- Genereertijd: 06 nov 2013 om 08:20
+-- Genereertijd: 29 nov 2013 om 07:34
 -- Serverversie: 5.5.24-log
 -- PHP-versie: 5.3.13
 
@@ -44,7 +44,7 @@ CREATE TABLE IF NOT EXISTS `accounts` (
 --
 
 INSERT INTO `accounts` (`id`, `account`, `password`, `email`, `profiel`, `adres`, `huisnummer`, `postcode`, `telefoon`) VALUES
-(1, 'Administrator', 'Admin1', 'admin1@admin.com', 1, 'Localhost', '1', '1234 AB', '0123-123456');
+(1, 'Administrator', 'admin1', 'admin1@admin.com', 1, 'Localhost', '1', '1234 AB', '0123-123456');
 
 -- --------------------------------------------------------
 
@@ -58,14 +58,60 @@ CREATE TABLE IF NOT EXISTS `content` (
   `pagina` text NOT NULL,
   `type` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Gegevens worden uitgevoerd voor tabel `content`
 --
 
 INSERT INTO `content` (`id`, `content`, `pagina`, `type`) VALUES
-(1, 'Test voor de inhoud van de homepage pagina waar om dit moment helemaal niks in staat wat door deze tekst dus gaat veranderen.', 'homepage', 'text');
+(1, 'hallo123', 'homepage', 'text'),
+(2, 'images/homepage/afbeelding1/admins_riding_me.png', 'homepage', 'image'),
+(3, 'images/homepage/afbeelding2/firstpenta.png', 'homepage', 'image');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `itemswebshop`
+--
+
+CREATE TABLE IF NOT EXISTS `itemswebshop` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `naam` text NOT NULL,
+  `waarde` int(11) NOT NULL,
+  `plaatje` text NOT NULL,
+  `menuitem` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Gegevens worden uitgevoerd voor tabel `itemswebshop`
+--
+
+INSERT INTO `itemswebshop` (`id`, `naam`, `waarde`, `plaatje`, `menuitem`) VALUES
+(1, 'test123', 150, 'images/webshop/brood.jpg', 'taarten'),
+(2, 'test2345', 10, 'images/webshop/taart.jpg', 'taarten');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `menuitemswebshop`
+--
+
+CREATE TABLE IF NOT EXISTS `menuitemswebshop` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `item` text NOT NULL,
+  `href` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Gegevens worden uitgevoerd voor tabel `menuitemswebshop`
+--
+
+INSERT INTO `menuitemswebshop` (`id`, `item`, `href`) VALUES
+(1, 'taarten', '?menuoptie=webshop&webshop=taarten'),
+(2, 'cake', '?menuoptie=webshop&webshop=cake');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
