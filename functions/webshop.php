7ec9f4f62daff02d0d6fc621842071a2a5f36c1b
arjen3112 <arjen.bovee@live.nl>
2013-12-04 09:20:35 +0100

    webshop shit

--- functions/webshop.php
+++ functions/webshop.php
@@ -9,7 +9,7 @@ function GetWebshop(){
     if(isset($_POST['wijzigitem'])){
         $output = webshopwijzigen();
     }
-    if(isset($_POST['bestellen'])){
+    if(isset($_POST['bestellen'])||isset($_POST['afrekenen'])){
         $output = bestellen();
     }
     return $output;
@@ -54,26 +54,39 @@ function loadwebshopitems(){
 
 function bestellen(){
     if (isset($_SESSION["profiel"]) && $_SESSION["profiel"] == "1"){
-    $q ='SELECT * FROM `itemswebshop` WHERE `menuitem` = "'.$_GET['webshop'].'" AND `id` = "'.$_POST["id"].'"';
-    $resultaat = mysql_query($q);
-    $output = '<div id="webshopbestelcontainer">';
-    while($row = mysql_fetch_array($resultaat)){
-        $output .= '<div class="webshopitem">
-                        <form method="post" action="">
-                        <table>
-                        <input type="hidden" name="id" value="'.$row["id"].'">
-                        <tr><td colspan="2"><img src="'.$row["plaatje"].'"></td>
-                        </tr>
-                        <tr><td>'.$row["naam"].'</td><td>&#128 '.$row["waarde"].'</td><td style="width:100%;"><input type="submit" name="afrekenen" value="afrekenen" style="width:100%;"></td></tr>
-                        </table>
-                        </form>
-                    </div>
-        ';
-    }
-    $output .= '</div>';
-    }else{
-        $output="U moet ingelogt zijn om iets te kunnen bestellen.";
-    }
+        if(isset($_POST['afrekenen'])){
+            $account = $_SESSION['username'];
+            $itemid = $_POST['id'];
+            $itemnaam = $_POST['naam'];
+            $waarde = $_POST['waarde'];
+            $status = "wordt verwerkt";
+            $output = "Uw bestelling wordt verwerkt!";
+            $q ='INSERT INTO `bestellingen`(account, itemid, itemnaam, waarde, status) VALUES("'.$account.'","'.$itemid.'","'.$itemnaam.'","'.$waarde.'","'.$status.'")'; 
+            mysql_query($q);   
+        }else{
+            $q ='SELECT * FROM `itemswebshop` WHERE `menuitem` = "'.$_GET['webshop'].'" AND `id` = "'.$_POST["id"].'"';
+            $resultaat = mysql_query($q);
+            $output = '<div id="webshopbestelcontainer">';
+            while($row = mysql_fetch_array($resultaat)){
+                $output .= '<div class="webshopitem">
+                                <form method="post" action="">
+                                <table>
+                                <input type="hidden" name="id" value="'.$row["id"].'">
+                                <input type="hidden" name="naam" value="'.$row["naam"].'">
+                                <input type="hidden" name="waarde" value="'.$row["waarde"].'">
+                                <tr><td colspan="2"><img src="'.$row["plaatje"].'"></td>
+                                </tr>
+                                <tr><td>'.$row["naam"].'</td><td>&#128 '.$row["waarde"].'</td><td style="width:100%;"><input type="submit" name="afrekenen" value="afrekenen" style="width:100%;"></td></tr>
+                                </table>
+                                </form>
+                            </div>
+                ';
+            }
+        }
+        $output .= '</div>';
+        }else{
+            $output="U moet ingelogt zijn om iets te kunnen bestellen.";
+        }
     return $output;
 }
 
