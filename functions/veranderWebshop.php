3bbd84b4db0b227ac4d8cf9564055f3fea9e3bf9
theblackgigant <reyno.tekoppele@gmail.com>
2013-12-06 08:33:51 +0100

    its a nigga!

--- functions/veranderWebshop.php
+++ functions/veranderWebshop.php
@@ -1,10 +1,6 @@
 <?php
 function webshopwijzigen() {
 	if (isset($_SESSION["profiel"]) && $_SESSION["profiel"] == "1") {
-		if (isset($_POST['wijziggegevens'])) {
-			if (!empty($_POST['naamproduct']) && !empty($_POST['prijsproduct']) && !$_FILES['afbeelding']['name'] == "")
-				$output = wijzigGegevens();
-		}
 		$q = 'SELECT * FROM `itemswebshop` WHERE `menuitem` = "' . $_GET['webshop'] . '" AND `id` = "' . $_POST["id"] . '"';
 		$resultaat = mysql_query($q);
 		$output = '<div id="webshopbestelcontainer">';
@@ -13,10 +9,11 @@ function webshopwijzigen() {
                         <form method="post" action="">
                         <table>
                         <input type="hidden" name="id" value="' . $row["id"] . '">
+                        
                         <tr><td><img src="' . $row["plaatje"] . '"></td>
 						<td><input type="file" value="Upload afbeelding" name="afbeelding"></td>
-                        <td><input type="submit" class="btnVeranderHomepage" name="wijziggegevens" value="Wijzig gegevens"></td>
-                        </tr>
+                        <td><input type="submit" class="btnVeranderHomepage" name="wijziggegevens" value="Wijzig gegevens"></td></tr>
+                        
                         <tr><td><input type="text" name="naamproduct" value="' . $row["naam"] . '"</td>
                         <td><input type="text" name="prijsproduct" value="&#128 ' . $row["waarde"] . '"</td><td style="width:100%;"></td></tr>
                         </table>
@@ -28,8 +25,15 @@ function webshopwijzigen() {
 	} else {
 		$output = "U moet ingelogt zijn om iets te kunnen bestellen.";
 	}
+	
+	if (isset($_POST['wijziggegevens'])) {
+			if (!empty($_POST['naamproduct']) && !empty($_POST['prijsproduct']) && !$_FILES['afbeelding']['name'] == "")
+				$output = wijzigGegevens();
+		}
+	return $output;
+}
 
-	function wijzigGegevens() {
+function wijzigGegevens() {
 		$files = glob('images/webshop/*');
 		foreach ($files as $file) {
 			if (is_file($file))
@@ -50,6 +54,3 @@ function webshopwijzigen() {
 			}
 		}
 	}
-
-	return $output;
-}
